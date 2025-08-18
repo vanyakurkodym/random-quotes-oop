fc012fcac8f2fbd0037b1585f43c598fe8deb5ac
Ivan Kurkodym <work.kurkodym@gmail.com>
2025-08-18 22:01:15 +0300

    Initial version of the quotes app using OOP and classes - chosing random quote from the array of quotes

---
+++ src/classes/Quote.js
@@ -0,0 +1,10 @@
+class Quote {
+    constructor (id, text, author){
+        this.id = id;
+        this.text = text;
+        this.author = author;
+        this.isFavourite = false;
+    }
+}
+
+export default Quote;
---
+++ src/utils/MathUtils.js
@@ -0,0 +1,17 @@
+
+class MathUtils {
+
+    static genarateRandomInt(maxInt){
+        return Math.floor(Math.random() * maxInt);
+    }
+
+}
+
+export default MathUtils;
+
+
+// function generateRandomInt(maxInt){
+//     return Math.floor(Math.random() * maxInt);
+// }
+
+// export {generateRandomInt};
