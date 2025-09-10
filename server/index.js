5b0d0bec93461fe632739a8450a9425451fc9390
Ivan Kurkodym <work.kurkodym@gmail.com>
2025-09-10 16:49:39 +0300

    Creating own quotes API Node.js Express server with resource /quotes/random-single

---
+++ server/index.js
@@ -0,0 +1,20 @@
+// import express from 'express'
+const express = require('express')
+const quotes = require('./data/quotes')
+
+const app = express()
+const PORT = 3000
+
+function getRandomQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    return quotes[randomIndex];
+}
+
+app.get('/quotes/random-single', (req, res) => {
+    const randomQuote = getRandomQuote();
+    res.json(randomQuote);
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+})
